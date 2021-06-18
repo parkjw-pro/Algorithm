12dbb2c871ed6f3f236e456efdbbda770a09b42c
parkjw-pro <64348332+parkjw-pro@users.noreply.github.com>
2021-06-18 23:18:23 +0900

    0618 알고리즘

---
+++ BaekJoon_Problem/Problem_2819.java
@@ -0,0 +1,52 @@
+package com.ssafy;
+import java.util.Scanner;
+
+public class Problem_2819 {
+	static int arr[][] = new int[4][4];
+
+	public static void main(String[] args) {
+		
+
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					arr[i][j] = sc.nextInt();}
+			}
+			
+			
+
+		}
+	}
+	
+	private static void count() {
+		
+		int answer[] = new int[7];
+		for (int i = 1; i < 3; i++) {
+			for (int j = 1; j < 3; j++) {
+				
+			
+				
+			}
+			
+		}
+		
+		
+		
+		
+	
+}
+	
+	
+	
+	
+}
+		
+		
+
